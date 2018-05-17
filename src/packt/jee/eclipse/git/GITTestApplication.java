08065bd8afa5ef8c3a93b692229ea2d008dbe42f
Yanness ALLABI <daerquyl@gmail.com>
2018-05-17 11:54:09 +0100

    Correction on bug done

--- src/packt/jee/eclipse/git/GITTestApplication.java
+++ src/packt/jee/eclipse/git/GITTestApplication.java
@@ -3,5 +3,6 @@ package packt.jee.eclipse.git;
 public class GITTestApplication {
     public static void main( String... args ) {
         System.out.println( "Yes" );
+        System.out.println( "Hello Git, from branch bug#1234 !!" );
     }
 }
